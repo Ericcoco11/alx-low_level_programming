07edfc9288b1ad5263007628871e495b59a135a6
Ericcoco11 <oloweeric@gmail.com>
2023-04-03 17:56:44 -0700

    locator

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,10 +1,28 @@
 #include "main.h"
 /**
- * _strstr -  finds the first occurrence of the 
+ * _strstr - finds the first occurrence of the
  * substring needle in the string haystack.
- * The terminating null bytes (\0) are not compared
- *
- * return: 
- *
+ * The terminating null bytes (\0) are not compared.
+ * @haystack: input
+ * @needle: input
+ * Return: always 0.
  */
+
 char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
