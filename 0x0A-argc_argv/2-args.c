6ef5c3bb9aee397260a4d2c2b1348612c0521c40
Ericcoco11 <oloweeric@gmail.com>
2023-04-10 16:16:27 -0700

    all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - prints all arguments it receives
  * @argc: argument count
  * @argv: arguments
  *
- * return: 0
+ * Return: Always 0
  */
 
 int main(int argc, char **argv)
